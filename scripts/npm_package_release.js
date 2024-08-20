74b638b54899229d64c3ae2517e16638562231d7
Fatehali Sunasara <fatehali429@hotmail.com>
2024-08-20 12:58:57 +0530

    Adding npm_package build script

---
+++ scripts/npm_package_release.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+var path = require('path');
+var shell = require('shelljs');
+
+var npm_package_path = path.resolve(__dirname, '../npm_package/');
+
+var libDir = path.join(path.resolve(process.cwd()), 'lib');
+var libEsm = path.join(path.resolve(process.cwd()), 'lib-esm');
+var release = path.join(path.resolve(process.cwd()), 'release');
+
+copyIntoRelease(libDir);
+copyIntoRelease(libEsm);
+copyIntoRelease(release);
+buildPackageJson();
+
+function copyIntoRelease(folderPath) {
+  shell.cp('-r', folderPath, npm_package_path);
+}
+
+function buildPackageJson() {
+  const outputPath = path.resolve(npm_package_path, 'package.json');
+  const packagePath = path.resolve(__dirname, '..', 'package.json');
+  const packageJson = JSON.parse(fs.readFileSync(packagePath));
+  packageJson.name = 'angular-ui-router-extended';
+  fs.writeFileSync(outputPath, JSON.stringify(packageJson, null, 2));
+}
